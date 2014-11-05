9d48b9e4396507d6617755dacd8bf11aa4734caf
Wendel Toews <wendel@dynamo-electric.com>
2014-11-05 14:14:07 -0700

    try to add vcs in package

--- TabularInput.php
+++ TabularInput.php
@@ -25,34 +25,17 @@ class TabularInput extends  Widget {
 
     public function run(){
         TabularAsset::register($this->view);
+        $table='';
+//<table class="footable table table-striped table-bordered">
+//  <thead>
+//    <tr>
+//      <th>Name</th>
+//      <th data-hide="phone">Phone</th>
+//      <th data-hide="phone,tablet">Email</th>
+//    </tr>
+//  </thead>
 
-        $table='
-        <table class="footable table table-striped table-bordered">
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th data-hide="phone">Phone</th>
-      <th data-hide="phone,tablet">Email</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Bob Builder</td>
-      <td>555-12345</td>
-      <td>bob@home.com</td>
-    </tr>
-    <tr>
-      <td>Bridget Jones</td>
-      <td>544-776655</td>
-      <td>bjones@mysite.com</td>
-    </tr>
-    <tr>
-      <td><input type="text">  </td>
-      <td>555-99911</td>
-      <td><input type="button">cruise1@crazy.com</td>
-    </tr>
-  </tbody>
-</table>';
+
 
 
         return $table;
